a17616c5cd35f8813b4b36b57e30bea387245837
Takeda-senpai <deadboltsx303@gmail.com>
2023-02-12 17:34:12 +0700

    Revert "Refactor sepolicy.rules resolve"
    This reverts commit 9e8c68af12815625fb47b1f972ddc687a3dd0df5.

--- scripts/boot_patch.sh
+++ scripts/boot_patch.sh
@@ -73,15 +73,11 @@ fi
 [ -z $KEEPFORCEENCRYPT ] && KEEPFORCEENCRYPT=false
 [ -z $PATCHVBMETAFLAG ] && PATCHVBMETAFLAG=false
 [ -z $RECOVERYMODE ] && RECOVERYMODE=false
-[ -z $ISENCRYPTED ] && ISENCRYPTED=false
 export KEEPVERITY
 export KEEPFORCEENCRYPT
 export PATCHVBMETAFLAG
-export ISENCRYPTED
 
 chmod -R 755 .
-
-RULESDEVICE="$(./magiskinit --rules-device)" || abort "! Unable to find rules partition!"
 
 #########
 # Unpack
@@ -156,7 +152,6 @@ echo "KEEPVERITY=$KEEPVERITY" > config
 echo "KEEPFORCEENCRYPT=$KEEPFORCEENCRYPT" >> config
 echo "PATCHVBMETAFLAG=$PATCHVBMETAFLAG" >> config
 echo "RECOVERYMODE=$RECOVERYMODE" >> config
-echo "RULESDEVICE=$RULESDEVICE" >> config
 [ ! -z $SHA1 ] && echo "SHA1=$SHA1" >> config
 
 # Compress to save precious ramdisk space
