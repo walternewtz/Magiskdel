rushizgithub/Magisk

add early-mount.d back

native/src/init/mount.cpp
@@ -154,13 +154,23 @@ static void mount_preinit_dir(string preinit_dev) {
     if (mounted || mount(PREINITDEV, PREINITMNT, "ext4", MS_RDONLY, nullptr) == 0 ||
         mount(PREINITDEV, PREINITMNT, "f2fs", MS_RDONLY, nullptr) == 0) {
         string preinit_dir = resolve_preinit_dir(PREINITMNT);
+        string early_mnt_dir = resolve_early_mount_dir(PREINITMNT);
         // Create bind mount
         xmkdirs(PREINITMIRR, 0);
+        xmkdirs(EARLYMNTRO, 0);
         if (access(preinit_dir.data(), F_OK)) {
             LOGW("empty preinit: %s\n", preinit_dir.data());
         } else {
             LOGD("preinit: %s\n", preinit_dir.data());
             xmount(preinit_dir.data(), PREINITMIRR, nullptr, MS_BIND, nullptr);
+        }
+        if (access(early_mnt_dir.data(), F_OK)) {
+            LOGW("empty mount dir: %s\n", early_mnt_dir.data());
+        } else {
+            LOGD("early mount: %s\n", early_mnt_dir.data());
+            xmount("early-mount.d", EARLYMNTRO, "tmpfs", 0, nullptr);
+            cp_afc(early_mnt_dir.data(), EARLYMNTRO);
+            xmount(nullptr, EARLYMNTRO, nullptr, MS_RDONLY | MS_REMOUNT, nullptr);
         }
         xumount2(PREINITMNT, MNT_DETACH);
     } else {
@@ -261,6 +271,40 @@ void BaseInit::prepare_data() {
     cp_afc("/overlay.d", "/data/overlay.d");
 }
 
+static bool is_symlink(const char *path){
+    struct stat st; return lstat(path, &st) == 0 && S_ISLNK(st.st_mode);
+}
+
+static void simple_mount(const string &sdir, const string &ddir = "") {
+    auto dir = xopen_dir(sdir.data());
+    if (!dir) return;
+    for (dirent *entry; (entry = xreaddir(dir.get()));) {
+        string src = sdir + "/" + entry->d_name;
+        string dest = ddir + "/" + entry->d_name;
+        if (access(dest.data(), F_OK) == 0 && !is_symlink(dest.data())) {
+        	if (entry->d_type == DT_LNK) continue;
+            else if (entry->d_type == DT_DIR) {
+                // Recursive
+                simple_mount(src, dest);
+            } else {
+                LOGD("bind_mnt: %s <- %s\n", dest.data(), src.data());
+                xmount(src.data(), dest.data(), nullptr, MS_BIND, nullptr);
+            }
+        }
+    }
+}
+
+static void early_mount() {
+    if (access(EARLYMNTRO "/system", F_OK) == 0)
+        simple_mount(EARLYMNTRO "/system", "/system");
+#define EARLY_MNT(part) \
+    if (access(EARLYMNTRO "/system/" part, F_OK) == 0 && !is_symlink("/" part)) \
+        simple_mount(EARLYMNTRO "/system/" part, "/" part);
+    EARLY_MNT("vendor")
+    EARLY_MNT("product")
+    EARLY_MNT("system_ext")
+}
+
 void MagiskInit::setup_tmp(const char *path) {
     LOGD("Setup Magisk tmp at %s\n", path);
     chdir("/data");
@@ -271,6 +315,7 @@ void MagiskInit::setup_tmp(const char *path) {
     xmkdir(WORKERDIR, 0);
 
     mount_preinit_dir(preinit_dev);
+    early_mount();
 
     cp_afc(".backup/.magisk", INTLROOT "/config");
     rm_rf(".backup");
@@ -281,6 +326,7 @@ void MagiskInit::setup_tmp(const char *path) {
     xsymlink("./magiskpolicy", "supolicy");
 
     xmount(".", path, nullptr, MS_BIND, nullptr);
+    xmount(EARLYMNTRO, (string(path) + "/" EARLYMNTRO).data(), nullptr, MS_BIND, nullptr);
 
     chdir("/");
 }

walternewtz/Magiskdel

native/src/init/mount.cpp
@@ -153,13 +153,23 @@ static void mount_preinit_dir(string preinit_dev) {
     if (mounted || mount(PREINITDEV, PREINITMNT, "ext4", MS_RDONLY, nullptr) == 0 ||
         mount(PREINITDEV, PREINITMNT, "f2fs", MS_RDONLY, nullptr) == 0) {
         string preinit_dir = resolve_preinit_dir(PREINITMNT);
+        string early_mnt_dir = resolve_early_mount_dir(PREINITMNT);
         // Create bind mount
         xmkdirs(PREINITMIRR, 0);
+        xmkdirs(EARLYMNT, 0);
         if (access(preinit_dir.data(), F_OK)) {
             LOGW("empty preinit: %s\n", preinit_dir.data());
         } else {
             LOGD("preinit: %s\n", preinit_dir.data());
             xmount(preinit_dir.data(), PREINITMIRR, nullptr, MS_BIND, nullptr);
+        }
+        if (access(early_mnt_dir.data(), F_OK)) {
+            LOGW("empty mount dir: %s\n", early_mnt_dir.data());
+        } else {
+            LOGD("early mount: %s\n", early_mnt_dir.data());
+            xmount("early-mount.d", EARLYMNT, "tmpfs", 0, nullptr);
+            cp_afc(early_mnt_dir.data(), EARLYMNT);
+            xmount(nullptr, EARLYMNT, nullptr, MS_RDONLY | MS_REMOUNT, nullptr);
         }
         xumount2(PREINITMNT, MNT_DETACH);
     } else {
@@ -257,6 +267,40 @@ void BaseInit::prepare_data() {
     cp_afc("/overlay.d", "/data/overlay.d");
 }
 
+static bool is_symlink(const char *path){
+    struct stat st; return lstat(path, &st) == 0 && S_ISLNK(st.st_mode);
+}
+
+static void simple_mount(const string &sdir, const string &ddir = "") {
+    auto dir = xopen_dir(sdir.data());
+    if (!dir) return;
+    for (dirent *entry; (entry = xreaddir(dir.get()));) {
+        string src = sdir + "/" + entry->d_name;
+        string dest = ddir + "/" + entry->d_name;
+        if (access(dest.data(), F_OK) == 0 && !is_symlink(dest.data())) {
+        	if (entry->d_type == DT_LNK) continue;
+            else if (entry->d_type == DT_DIR) {
+                // Recursive
+                simple_mount(src, dest);
+            } else {
+                LOGD("bind_mnt: %s <- %s\n", dest.data(), src.data());
+                xmount(src.data(), dest.data(), nullptr, MS_BIND, nullptr);
+            }
+        }
+    }
+}
+
+static void early_mount() {
+    if (access(EARLYMNT "/system", F_OK) == 0)
+        simple_mount(EARLYMNT "/system", "/system");
+#define EARLY_MNT(part) \
+    if (access(EARLYMNT "/system/" part, F_OK) == 0 && !is_symlink("/" part)) \
+        simple_mount(EARLYMNT "/system/" part, "/" part);
+    EARLY_MNT("vendor")
+    EARLY_MNT("product")
+    EARLY_MNT("system_ext")
+}
+
 void MagiskInit::setup_tmp(const char *path) {
     LOGD("Setup Magisk tmp at %s\n", path);
     chdir("/data");
@@ -267,6 +311,7 @@ void MagiskInit::setup_tmp(const char *path) {
     xmkdir(WORKERDIR, 0);
 
     mount_preinit_dir(preinit_dev);
+    early_mount();
 
     cp_afc(".backup/.magisk", MAIN_CONFIG);
     rm_rf(".backup");
@@ -277,6 +322,7 @@ void MagiskInit::setup_tmp(const char *path) {
     xsymlink("./magiskpolicy", "supolicy");
 
     xmount(".", path, nullptr, MS_BIND, nullptr);
+    xmount(EARLYMNT, (string(path) + "/" EARLYMNT).data(), nullptr, MS_BIND, nullptr);
 
     chdir("/");
 }